3739b017d144c6a34958619ea3ec5271c39ccbd8
DrewPsi <Drewsteindl@icloud.com>
2022-09-08 01:11:03 -0400

    quick

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
     // Start is called before the first frame update
+
     void Start() {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
     }
+    public float minX = -60f;
+	public float maxX = 60f;
 
-    private int speed = 32;
-    // Update is called once per frame
-    public float speedH = 2.0f;
-    public float speedV = 2.0f;
+	public float sensitivity;
+	public Camera cam;
 
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
+	float rotY = 0f;
+	float rotX = 0f;
+    
 
     void Update () {
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
+        rotY += Input.GetAxis("Mouse X") * sensitivity;
+		rotX += Input.GetAxis("Mouse Y") * sensitivity;
+		rotX = Mathf.Clamp(rotX, minX, maxX);
 
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-    }
+		//cam.transform.localEulerAngles = new Vector3(0, rotY, 0);
+		cam.transform.localEulerAngles = new Vector3(-rotX, rotY, 0);
+
+		if (Input.GetKeyDown(KeyCode.Escape)){
+        	//Mistake happened here vvvv
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+
+		if (Cursor.visible && Input.GetMouseButtonDown(1))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
 }
--- Assets/lever.cs
+++ Assets/lever.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class lever : MonoBehaviour
 {
-    
+    private string lastState = "backward";
     // Start is called before the first frame update
     void Start()
     {
+        transform.localRotation = Quaternion.Euler(-90, 0, 0);
         
     }
 
@@ -17,17 +18,26 @@ public class lever : MonoBehaviour
 
     }
     void OnMouseDown(){
+            
             if (transform.localRotation ==  Quaternion.Euler(-90, 0, 0)){
-                transform.localRotation = Quaternion.Euler(-45, 0, 0);
+                if (lastState == "forward"){
+                    transform.localRotation = Quaternion.Euler(-135, 0, 0);
+
+                }
+                else if(lastState == "backward"){
+                    transform.localRotation =  Quaternion.Euler(-45, 0, 0);
+                }
+
 
 
             }
             else if(transform.localRotation ==  Quaternion.Euler(-45, 0, 0)){
-                transform.localRotation = Quaternion.Euler(-135, 0, 0);
-
+                transform.localRotation = Quaternion.Euler(-90, 0, 0);
+                lastState="forward";
             }
             else{
                 transform.localRotation = Quaternion.Euler(-90, 0, 0);
+                lastState="backward";
 
 
             }
